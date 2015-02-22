8a7b76f00fa4365fb0aec438f8db0cbde71449ef
Jarvist Moore Frost <jarvist@gmail.com>
2015-02-22 10:48:02 +0000

    Starter files for MD with VASP on MAPI/FAPI etc.; for Jonathan / Balena

---
+++ 2015-02_JMF_MAPI_FAPI_Supercells/build_supercells_ase.py
@@ -0,0 +1,21 @@
+# JMF 2015-02-22
+# Quick ASE script to generate large supercells for MAPI and FAPI
+
+import ase.io.vasp
+
+cell = ase.io.vasp.read_vasp("pseudocubic_CH3NH3PbI3.POSCAR.vasp")
+
+ase.io.vasp.write_vasp("POSCAR_6x6x6_MAPI",cell*(4,4,4), label='POSCAR_4x4x4_MAPI',direct=True,sort=True,vasp5=True)
+ase.io.vasp.write_vasp("POSCAR_6x6x6_MAPI",cell*(6,6,6), label='POSCAR_6x6x6_MAPI',direct=True,sort=True,vasp5=True)
+ase.io.vasp.write_vasp("POSCAR_8x8x8_MAPI",cell*(8,8,8), label='POSCAR_8x8x8_MAPI',direct=True,sort=True,vasp5=True)
+
+cell = ase.io.vasp.read_vasp("pseudocubic_CH2NHCH2PbI3.POSCAR.vasp")
+
+ase.io.vasp.write_vasp("POSCAR_4x4x4_FAPI",cell*(6,6,6), label='POSCAR_4x4x4_FAPI',direct=True,sort=True,vasp5=True)
+ase.io.vasp.write_vasp("POSCAR_6x6x6_FAPI",cell*(6,6,6), label='POSCAR_6x6x6_FAPI',direct=True,sort=True,vasp5=True)
+ase.io.vasp.write_vasp("POSCAR_8x8x8_FAPI",cell*(8,8,8), label='POSCAR_8x8x8_FAPI',direct=True,sort=True,vasp5=True)
+
+
+# Files from github;
+# https://github.com/WMD-Bath/Hybrid-perovskites/
+# pseudocubic_CH2NHCH2PbI3.POSCAR.vasp  pseudocubic_CH3NH3PbI3.POSCAR.vasp
