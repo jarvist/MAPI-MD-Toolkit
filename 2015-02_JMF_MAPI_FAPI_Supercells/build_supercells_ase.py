fed171f805c4dee7c97003dcaa653c4874aace22
Jarvist Moore Frost <jarvist@gmail.com>
2016-02-10 21:21:46 +0000

    Expand range of files... correct error in 4x4x4-FAPI (was actually 6x6x6!)

--- 2015-02_JMF_MAPI_FAPI_Supercells/build_supercells_ase.py
+++ 2015-02_JMF_MAPI_FAPI_Supercells/build_supercells_ase.py
@@ -5,15 +5,19 @@ import ase.io.vasp
 
 cell = ase.io.vasp.read_vasp("pseudocubic_CH3NH3PbI3.POSCAR.vasp")
 
+ase.io.vasp.write_vasp("POSCAR_2x2x2_MAPI",cell*(2,2,2), label='POSCAR_2x2x2_MAPI',direct=True,sort=True,vasp5=True)
 ase.io.vasp.write_vasp("POSCAR_4x4x4_MAPI",cell*(4,4,4), label='POSCAR_4x4x4_MAPI',direct=True,sort=True,vasp5=True)
 ase.io.vasp.write_vasp("POSCAR_6x6x6_MAPI",cell*(6,6,6), label='POSCAR_6x6x6_MAPI',direct=True,sort=True,vasp5=True)
 ase.io.vasp.write_vasp("POSCAR_8x8x8_MAPI",cell*(8,8,8), label='POSCAR_8x8x8_MAPI',direct=True,sort=True,vasp5=True)
+ase.io.vasp.write_vasp("POSCAR_10x10x10_MAPI",cell*(10,10,10), label='POSCAR_10x10x10_MAPI',direct=True,sort=True,vasp5=True)
 
 cell = ase.io.vasp.read_vasp("pseudocubic_CH2NHCH2PbI3.POSCAR.vasp")
 
-ase.io.vasp.write_vasp("POSCAR_4x4x4_FAPI",cell*(6,6,6), label='POSCAR_4x4x4_FAPI',direct=True,sort=True,vasp5=True)
+ase.io.vasp.write_vasp("POSCAR_2x2x2_FAPI",cell*(2,2,2), label='POSCAR_2x2x2_FAPI',direct=True,sort=True,vasp5=True)
+ase.io.vasp.write_vasp("POSCAR_4x4x4_FAPI",cell*(4,4,4), label='POSCAR_4x4x4_FAPI',direct=True,sort=True,vasp5=True)
 ase.io.vasp.write_vasp("POSCAR_6x6x6_FAPI",cell*(6,6,6), label='POSCAR_6x6x6_FAPI',direct=True,sort=True,vasp5=True)
 ase.io.vasp.write_vasp("POSCAR_8x8x8_FAPI",cell*(8,8,8), label='POSCAR_8x8x8_FAPI',direct=True,sort=True,vasp5=True)
+ase.io.vasp.write_vasp("POSCAR_10x10x10_FAPI",cell*(10,10,10), label='POSCAR_10x10x10_FAPI',direct=True,sort=True,vasp5=True)
 
 
 # Files from github;
